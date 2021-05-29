3670cfb384d4f1998322c9bc99ccc8f54cd6e133
Heesung Yang <leibniz21c@gmail.com>
2021-05-30 05:49:15 +0900

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,40 +1,42 @@
 const express = require('express')
 const ejs = require('ejs')
+const sequelize = require('./models/index').sequelize;
 const app = new express()
+const bodyParser = require('body-parser')
+
+sequelize.sync()
 
 // App Using
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+// MySQL DB
+// 도커 컨테이너 생성 전까지만 설정파일로 관리합시당
 
-// MySQL
-// Must be protected! (얘들아 여기 노출안되게 잘 확인해 꼭! - 괴도 희성)
-const mysql = require('mysql2')
-var connection = mysql.createConnection({
-    host     : '***.***.***.***',
-    user     : '********',
-    password : '********',
-    database : 'LEGO'
-});
-connection.connect();
+// Routers
+const homeRouter = require('./routes/home')
+const signupRouter = require('./routes/signup')
+const gamesRouter = require('./routes/games')
+const rankingRouter = require('./routes/ranking')
+const communityRouter = require('./routes/community')
+const developerRouter = require('./routes/developer')
 
 // Controllers
-const homeController = require('./controllers/home')
-const signupController = require('./controllers/signup')
-const gamesController = require('./controllers/games')
-const rankingController = require('./controllers/ranking')
-const communityController = require('./controllers/community')
-const developerController = require('./controllers/developer')
+const storeUserController = require('./controllers/storeUserController')
+
+// Middlewares
+const autuMiddleware = require('./middlewares/authMiddleware')
 
 // Routing
-app.get('/', homeController)
-app.get('/signup', signupController)
-app.get('/games', gamesController)
-app.get('/ranking', rankingController)
-app.get('/community', communityController)
-app.get('/developer', developerController)
+app.get('/', homeRouter)
+app.get('/signup', signupRouter)
+app.get('/games', gamesRouter)
+app.get('/ranking', rankingRouter)
+app.get('/community', communityRouter)
+app.get('/developer', developerRouter)
 
+app.post('/signup', autuMiddleware, storeUserController)
 // Listening
 app.listen(3000, () => {
     console.log('App listening on http://127.0.0.1:3000/')
