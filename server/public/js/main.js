eaf70a1ea8cbe68be873daa03bae5fbffb08f731
Heesung Yang <leibniz21c@gmail.com>
2021-05-31 02:58:35 +0900

    Update main style and script

--- server/public/js/main.js
+++ server/public/js/main.js
@@ -28,85 +28,11 @@
 			}, 100);
 		});
 
-	// Slideshow Background.
-		// (function() {
-
-		// 	// Settings.
-		// 		var settings = {
-
-		// 			// Images (in the format of 'url': 'alignment').
-		// 				images: {
-		// 					'images/login_background.mov': 'center',
-		// 					'images/bg02.jpg': 'center',
-		// 					'images/bg03.jpg': 'center'
-		// 				},
-
-		// 			// Delay.
-		// 				delay: 6000
-
-		// 		};
-
-		// 	// Vars.
-		// 		var	pos = 0, lastPos = 0,
-		// 			$wrapper, $bgs = [], $bg,
-		// 			k, v;
-
-		// 	// Create BG wrapper, BGs.
-		// 		$wrapper = document.createElement('div');
-		// 			$wrapper.id = 'bg';
-		// 			$body.appendChild($wrapper);
-
-		// 		for (k in settings.images) {
-
-		// 			// Create BG.
-		// 				$bg = document.createElement('div');
-		// 					$bg.style.backgroundImage = 'url("' + k + '")';
-		// 					$bg.style.backgroundPosition = settings.images[k];
-		// 					$wrapper.appendChild($bg);
-
-		// 			// Add it to array.
-		// 				$bgs.push($bg);
-
-		// 		}
-
-		// 	// Main loop.
-		// 		$bgs[pos].classList.add('visible');
-		// 		$bgs[pos].classList.add('top');
-
-		// 		// Bail if we only have a single BG or the client doesn't support transitions.
-		// 			if ($bgs.length == 1
-		// 			||	!canUse('transition'))
-		// 				return;
-
-		// 		window.setInterval(function() {
-
-		// 			lastPos = pos;
-		// 			pos++;
-
-		// 			// Wrap to beginning if necessary.
-		// 				if (pos >= $bgs.length)
-		// 					pos = 0;
-
-		// 			// Swap top images.
-		// 				$bgs[lastPos].classList.remove('top');
-		// 				$bgs[pos].classList.add('visible');
-		// 				$bgs[pos].classList.add('top');
-
-		// 			// Hide last image after a short delay.
-		// 				window.setTimeout(function() {
-		// 					$bgs[lastPos].classList.remove('visible');
-		// 				}, settings.delay / 2);
-
-		// 		}, settings.delay);
-
-		// })();
-
-	// Signup Form.
+	// Signin Form.
 		(function() {
-
 			// Vars.
-				var $form = document.querySelectorAll('#signup-form')[0],
-					$submit = document.querySelectorAll('#signup-form input[type="submit"]')[0],
+				var $form = document.querySelectorAll('#signin-form')[0],
+					$submit = document.querySelectorAll('#signin-form input[type="submit"]')[0],
 					$message;
 
 			// Bail if addEventListener isn't supported.
@@ -137,6 +63,8 @@
 			// Events.
 			// Note: If you're *not* using AJAX, get rid of this event listener.
 				$form.addEventListener('submit', function(event) {
+
+					console.log("[DEBUG] : Success")
 
 					event.stopPropagation();
 					event.preventDefault();
